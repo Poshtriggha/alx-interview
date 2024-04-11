03943be1917bcc58cd24f72b88d339c9477f359e
posh <mrposhboo@gmail.com>
2024-04-12 00:02:12 +0100

    origin

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle for n rows.
+    """
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle.
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+print_triangle(pascal_triangle(5))
