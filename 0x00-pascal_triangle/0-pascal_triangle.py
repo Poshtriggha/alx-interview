0fc297c3a31f786cfe90564ad588445b6ccaeca8
posh <mrposhboo@gmail.com>
2024-04-15 00:53:12 +0100

    PEP8 validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Generates Pascal's Triangle."""
+"""Pascal Triangle Interview Challenge"""
+
 
 def pascal_triangle(n):
-    """Generates a list of lists of numbers representing Pascal's Triangle."""
+    """Returns a list of lists of numbers
+    representing the Pascal triangle."""
     if n <= 0:
         return []
 
@@ -13,7 +15,8 @@ def pascal_triangle(n):
         line = [0] * (i + 1)
         line[0] = 1
         line[-1] = 1
-         
+        
+        
         for j in range(1, i):
             a = pascal_triangle[i - 1][j]
             b = pascal_triangle[i - 1][j - 1]
