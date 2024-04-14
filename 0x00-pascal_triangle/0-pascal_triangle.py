bd5d14af1cd9530d4339881dbfafe71b3130e75f
posh <mrposhboo@gmail.com>
2024-04-15 00:29:49 +0100

    indics

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,6 +7,7 @@ def pascal_triangle(n):
     row = [1]
 
     for j in range(n):
+        # define a row and fill first and last idx with 1
         row.append((row[j] * (n - j) // (j + 1)))
 
     return row
@@ -20,6 +21,7 @@ def print_triangle(n):
     triangle = []
 
     for i in range(n):
+        #Calculate the values between the first and last indices 
         triangle.append(pascal_triangle(i + 1))
 
     return triangle
