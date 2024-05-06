41e2b5a3b41582b86bc1c2166086a5791fb72f08
posh <mrposhboo@gmail.com>
2024-05-06 22:23:33 +0100

    popular

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Simple Helper Function module
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+
+    Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple[int, int]: A tuple containing the start index and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+if __name__ == "__main__":
+    res = index_range(1, 7)
+    print(type(res))
+    print(res)
+
+    res = index_range(page=3, page_size=15)
+    print(type(res))
+    print(res)
