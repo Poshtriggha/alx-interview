7c3a7789ff36e082eb7a482aef56812e6d3b7d73
posh <mrposhboo@gmail.com>
2024-05-09 21:48:17 +0100

    validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,56 +1,28 @@
 #!/usr/bin/python3
-"""
-Module for validating UTF-8 encoding.
-"""
+"""a method that determines if a given data set represents
+a valid UTF-8 encoding"""
 
 
 def validUTF8(data):
-    """
-    Determines if a given data set represents a valid UTF-8 encoding.
+    """Determines whether given data set represents a valid UTF-8"""
+    expected_len = 0
 
-    Args:
-        data: A list of integers representing bytes of data.
+    for n in data:
+        if expected_len == 0:
+            if n >> 5 == 0b110 or n >> 5 == 0b1110:
 
-    Returns:
-        True if data is a valid UTF-8 encoding, else return False.
-    """
-    def countLeadingOnes(byte):
-        count = 0
-        mask = 1 << 7
-        while byte & mask:
-            count += 1
-            mask >>= 1
-        return count
+                expected_len = 1
+            elif n >> 4 == 0b1110:
+                expected_len = 2
+            elif n >> 3 == 0b11110:
+                expected_len = 3
 
-    i = 0
-    while i < len(data):
-        leading_ones = countLeadingOnes(data[i])
         
-        if leading_ones != 0 or (data[i] >> 6) == 2:
-            return False
-        
-        elif leading_ones == 0 or leading_ones > 4:
-            i += 1
-            continue
-        
-        if i + leading_ones > len(data):
-            return False
-        
-        for j in range(1, leading_ones):
-            if (data[i+j] >> 6) != 2:
+            elif n >> 7 == 0b1:
                 return False
-        
-        i += leading_ones
-    
-    return True
-
-
-if __name__ == "__main__":
-    data = [65]
-    print(validUTF8(data))
-
-    data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-    print(validUTF8(data))
-
-    data = [229, 65, 127, 256]
-    print(validUTF8(data))
+        else:
+            if n >> 6 != 0b10:
+                return False
+            
+            expected_len -= 1
+    return expected_len == 0
