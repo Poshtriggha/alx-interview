2afc7b66f71a5b7a33095f551603602c0e89d3b8
posh <mrposhboo@gmail.com>
2024-05-09 21:49:35 +0100

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""a method that determines if a given data set represents
-a valid UTF-8 encoding"""
+"""Represents a valid UTF-8 encoding"""
 
 
 def validUTF8(data):
-    """Determines whether given data set represents a valid UTF-8"""
+    """Represents a valid UTF-8"""
     expected_len = 0
 
     for n in data:
